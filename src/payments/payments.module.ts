8e8f7c880bcffb7563437a3a6e8b4ed71708eb0c
systemselectrotec-hash <systemselectrotec@gmail.com>
2026-05-04 11:49:12 -0300

    orders sin logica financiera 1

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -4,7 +4,6 @@ import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { Payment } from './entities/payment.entity';
 import { Order } from '../orders/entities/order.entity';
-import { OrdersModule } from '../orders/orders.module';
 import { UsersModule } from '../users/users.module';
 import { CashModule } from '../cash/cash.module';
 import { AccountModule } from 'src/account/account.module';
@@ -12,7 +11,6 @@ import { AccountModule } from 'src/account/account.module';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Payment, Order]),
-    OrdersModule, // para poder inyectar OrdersService y validar orden
     UsersModule, // para validar usuario que paga
     CashModule,
     AccountModule
